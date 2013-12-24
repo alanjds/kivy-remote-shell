c78dc7850131fe636ac2fcf2e97553aaa6666f27
Alan Justino <alan.justino@yahoo.com.br>
2013-12-24 13:20:08 -0200

    SL4A's android.Android code imported

--- sl4a_compat/android.py
+++ sl4a_compat/android.py
@@ -20,21 +20,20 @@ import os
 import socket
 import sys
 
-PORT = os.environ.get('AP_PORT')
-HOST = os.environ.get('AP_HOST')
-HANDSHAKE = os.environ.get('AP_HANDSHAKE')
+
 Result = collections.namedtuple('Result', 'id,result,error')
 
 
 class Android(object):
 
-  def __init__(self, addr=None):
-    if addr is None:
-      addr = HOST, PORT
+  def __init__(self, host=None, port=None, handshake=None):
+    addr = (host or os.environ.get('AP_HOST'),
+            port or os.environ.get('AP_PORT'))
     self.conn = socket.create_connection(addr)
     self.client = self.conn.makefile()
     self.id = 0
-    if HANDSHAKE is not None:
+    handshake = handshake or os.environ.get('AP_HANDSHAKE')
+    if handshake:
       self._authenticate(HANDSHAKE)
 
   def _rpc(self, method, *args):
@@ -60,5 +59,6 @@ class Android(object):
 
 
 class FacadeManagerProxy(object):
-  mFacadeManagerFactory = FacadeManagerFactory(FacadeConfiguration.getSdkLevel(), service, intent,
+  def __init__(self):
+    mFacadeManagerFactory = FacadeManagerFactory(FacadeConfiguration.getSdkLevel(), service, intent,
             FacadeConfiguration.getFacadeClasses())

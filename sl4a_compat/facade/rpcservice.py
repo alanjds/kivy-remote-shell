ba5bf5d80c1cee493070c0609af9923b30f5ba14
Alan Justino <alan.justino@yahoo.com.br>
2013-12-28 12:43:36 -0200

    rpcservice.start() implemented

--- sl4a_compat/facade/rpcservice.py
+++ sl4a_compat/facade/rpcservice.py
@@ -1,5 +1,55 @@
 #coding: utf-8
+from jnius import autoclass, cast, JavaException
+from android.runnable import run_on_ui_thread
+from android.broadcast import BroadcastReceiver
+import time
 
-def start(public=False, handshake=False, external=True):
-    raise NotImplementedError('Should start an RPC and return (host, port, handshake) tuple')
+Log = autoclass('com.googlecode.android_scripting.Log')
+Intent = autoclass('android.content.Intent')
+MinimalService = autoclass('org.alanjds.sl4acompat.MinimalService')
+
+activity = autoclass('org.renpy.android.PythonActivity').mActivity
+context = cast('android.content.Context', activity)
+
+
+def start(external=False):
+    host = port = received_handshake = None
+
+    if external:
+        # Public: $ am start -a com.googlecode.android_scripting.action.LAUNCH_SERVER -n com.googlecode.android_scripting/.activity.ScriptingLayerServiceLauncher --ez com.googlecode.android_scripting.extra.USE_PUBLIC_IP true
+        # Private on port: $ am start -a com.googlecode.android_scripting.action.LAUNCH_SERVER -n com.googlecode.android_scripting/.activity.ScriptingLayerServiceLauncher --ei com.googlecode.android_scripting.extra.USE_SERVICE_PORT 45001
+        # Private random: $ am start -a com.googlecode.android_scripting.action.LAUNCH_SERVER -n com.googlecode.android_scripting/.activity.ScriptingLayerServiceLauncher
+        raise NotImplementedError('Should call stock SL4A to start the RPC service')
+
+    netaddress_receiver = None
+    netaddress_received = False
+    receivedExtras = None
+
+    def _on_netaddress_received(context, intent):
+        netaddress_received = True
+        receivedExtras = intent.getExtras()
+        #headset_state = bool(extras.get('state'))
+        msg = 'NETADDRESS received!'; Log.v(msg); print msg
+        #netaddress_receiver.stop() # Disposable BroadcastReceiver?
+
+    netaddress_receiver = BroadcastReceiver(_on_netaddress_received,
+                                            actions=['org.alanjds.sl4acompat.STORE_RPC_NETADDRESS'])
+    netaddress_receiver.start()
+
+    # TODO: How to get MinimalService.class with pyjnius without this hack?
+    MinimalService__class = MinimalService().getClass()
+    serviceIntent = Intent(context, MinimalService__class)
+    componentName  = context.startService(serviceIntent)
+
+    try:
+        for i in xrange(20): # wait up to 10 sec
+            if netaddress_received:
+                break
+            time.sleep(0.5)
+        else:
+            raise RuntimeError('Service net address has requested but never received')
+    finally:
+        netaddress_receiver.stop()
+
+    return receivedExtras.getString('host'), receivedExtras.get('port'), receivedExtras.getString('handshake')
     return host, port, handshake
